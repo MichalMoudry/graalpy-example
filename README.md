6ed9b004afb2e88d2f74205ed96e545986f8d4c6
Michal Moudrý <moudry.michal@live.com>
2025-12-02 12:32:02 +0100

    Added a template for an index page

---
+++ README.md
@@ -0,0 +1,19 @@
+# GraalPy example
+
+## Relevant commands
+### Creating a virtual environment
+```
+uv venv .venv --python graalpy-3.12.8
+```
+### Synchronizing project's deps
+```
+uv sync
+```
+
+### Running the app
+Debug:
+```
+flask --app main run
+```
+
+Docker:
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+@app.route("/")
+def index() -> str:
+    return render_template(
+        "index.html",
+        current_time=datetime.now().isoformat()
+    )
+
+
 @app.route("/health")
-def health():
+def health() -> str:
     return "healthy"
